6869f8944d4c59466f40255ff3113f318f818021
Jag Bains <git@jagbains.org>
2025-02-05 16:09:33 +0000

    untested

--- day05/2.py
+++ day05/2.py
@@ -1,4 +1,5 @@
 import sys
+from collections import deque
 import re
 
 file="input.txt"
@@ -40,39 +41,39 @@ ranges=set()
 for i in range(0,len(seeds)-1,2):
     bottom=seeds[i]
     top=seeds[i+1]+bottom
-    ranges.add((bottom,top))
+    ranges.add((bottom,bottom,seeds[i+1]))
     total+=top
 print(total)
 print(ranges)
 
 #This logic is broken, I need to keep track of how each range maps to the next layer, so I can follow back from the lowest location range to a seed.
 #This should also probably be global? I can start with all the starting ranges, and build out from there
-for bottom,top in ranges:
-    xrange=set()
-    xrange.add((bottom,top))
-    print (bottom,top)
-    for xmap in maps:
-        rnext=set()
-        redo=set()
-        remove=set()
+# for each mapping layer:
+# take the current list of ranges and push to a heap (seed,mapped,range?)
+# while the heap has entries:
+# pop a range, check against the map, break it down when it overlaps a map boundary. add processed ranges to a temp set of ranges (as (seed,mapped,range) tuple), and unprocessed to the heap
+# finish by copying the temp set to the set of ranges
+
+for maplayer in maps:
+    heap=deque()
+    tempranges=set()
+    for r in ranges:
+        heap.append(r)
+    while len(heap>0):
+        s,b,o = heap.popleft()
         for l,h,offset in xmap:
-            for b,t in xrange:
-                if l <= b <= h:
-                    remove.add((b,t))
-                    if t <= h:
-                        nb=b+offset
-                        nt=t+offset
-                        rnext.add((nb,nt))
-                    elif t > h:
-                        nb=b+offset
-                        nt=h+offset
-                        rnext.add((nb,nt))
-                        nb=h+1
-                        nt=t
-                        redo.add((nb,nt))
-            xrange=xrange-remove
-            xrange=xrange+redo
-        xrange=rnext
+            if l <= b <= h:
+                if b+o <= h:
+                    nb=b+offset
+                    tempranges.add((s,nb,o))
+                elif t > h:
+                    nb=b+offset
+                    no=h-l
+                    tempranges.add((s,nb,no))
+                    nb=h+1
+                    no=o-(h-l)
+                    heap.append((s,nb,no))
+    ranges=set(tempranges)
 
 #    for seed in range(bottom,top):
 #        location=seed
