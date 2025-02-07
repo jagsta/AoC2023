4a06191a4f5167561c490350b82be757d91d2e0a
Jag Bains <git@jagbains.org>
2025-02-07 11:41:38 +0000

    part 2 complete

--- day07/2.py
+++ day07/2.py
@@ -24,30 +24,34 @@ fives={}
 for hand,bid in cards.items():
     print(hand)
     this={}
+    bump=0
     for card in hand:
         if card in this:
             this[card]+=1
         else:
             this[card]=1
+    if "J" in this:
+        bump=this["J"]
+        this["J"]=0
     sets =list(reversed(sorted(this.values())))
     print(sets)
-    if sets[0]==5:
+    if sets[0]+bump==5:
         fives[hand]=bid
-    elif sets[0]==4:
+    elif sets[0]+bump==4:
         fours[hand]=bid
-    elif sets[0]==3 and sets[1]==2:
+    elif sets[0]+bump==3 and sets[1]==2:
         houses[hand]=bid
-    elif sets[0]==3:
+    elif sets[0]+bump==3:
         threes[hand]=bid
-    elif sets[0]==2 and sets[1]==2:
+    elif sets[0]+bump==2 and sets[1]==2:
         twopairs[hand]=bid
-    elif sets[0]==2:
+    elif sets[0]+bump==2:
         pairs[hand]=bid
     else:
         highs[hand]=bid
 
 def sortCA(arr):
-    customAl='23456789TJQKA'
+    customAl='J23456789TQKA'
     dt = {c: i for i, c in enumerate(customAl)}
     return sorted(arr, key=lambda x: [dt[c] for c in x])
 
