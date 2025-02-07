7610455eef1faf25a9fbd23ab48eaefc7da519b3
Jag Bains <git@jagbains.org>
2025-02-07 13:58:25 +0000

    part 1 complete

--- day08/1.py
+++ day08/1.py
@@ -8,6 +8,7 @@ if len(sys.argv)>1:
 
 f=open(file)
 
+maps={}
 directions=[]
 for line in f.readlines():
     match=re.match(r'^([LR])+$',line)
@@ -17,6 +18,20 @@ for line in f.readlines():
             directions.append(c)
     match=re.match(r'(\w+) = \((\w+), (\w+)\)',line)
     if match:
-        print (match.group(1),match.group(2),match.group(3))
+        maps[match.group(1)]={"L":match.group(2),"R":match.group(3)}
+        #print (match.group(1),match.group(2),match.group(3))
 
+print (maps)
 print (directions)
+
+count=0
+n="AAA"
+found=False
+while not found:
+    for d in directions:
+        n=maps[n][d]
+        count+=1
+        if n=="ZZZ":
+            found=True
+
+print(count)
