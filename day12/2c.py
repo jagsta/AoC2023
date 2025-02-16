6778ed3aa999f91d38549b739159e143731270da
Jag Bains <git@jagbains.org>
2025-02-16 12:10:14 +0000

    Finally, working with recursion and positive and negative caching

--- day12/2c.py
+++ day12/2c.py
@@ -3,6 +3,7 @@ import re
 from itertools import combinations
 from itertools import permutations
 from itertools import product
+from collections import deque
 
 
 file="input.txt"
@@ -28,111 +29,95 @@ for line in f.readlines():
         lines[l]["blocks"].append(int(n))
     l+=1
 
-def perm(string,strindex,blocks,blindex):
+def rec(string,s,blocks,b):
+    if (s,b) in cache:
+        print(f'hit cache for {s},{b}: {cache[(s,b)]}')
+        return cache[(s,b)]
     count=0
-    if blindex<len(blocks):
-        blocksize = blocks[blindex]
-        matchstring="([?#]{"+str(blocksize)+"}[.?]+?)"
-        print(strindex,blindex,matchstring)
-        match=re.match(matchstring,string[strindex:])
-        if match:
-            count+=perm(string,strindex+match.end(),blocks,blindex+1)
+    if b==len(blocks) and string[s:].count("#")==0:
+        print(f'Solution found')
+        return 1
+    if (s,b) in misses:
+        return 0
+    if b==0 and string[:s].count("#")>0:
+        print(f'shifted past first block')
+        return 0
+    if sum(blocks[b:])+len(blocks)-b-1>len(string[s:]):
+        misses.add((s,b))
+        print(f'not enough string left:{s},{b} need {sum(blocks[b:])+len(blocks)-b-1}, got {len(string[s:])}')
+        return 0
+    if b<len(blocks):
+        blocksize = blocks[b]
+        if b==len(blocks)-1:
+            matchstring="(?=(^[?#]{"+str(blocksize)+"})([.?]*$))"
         else:
-            return 0
+            matchstring="(?=(^[?#]{"+str(blocksize)+"}[.?]))"
+            #print(s,b,matchstring,string[s:])
+        match=re.match(matchstring,string[s:])
+        if match:
+        #print(s,b,match.group(1),len(match.group(1)))
+            if b==len(blocks)-1:
+                count+=rec(string,s+blocksize+len(match.group(2)),blocks,b+1)
+            else:
+                count+=rec(string,s+blocksize+1,blocks,b+1)
+        if string[s]!="#":
+            count+=rec(string,s+1,blocks,b)
     else:
-       count+=1
+        misses.add(s,b)
+        return 0
+    cache[(s,b)]=count
     return count
 
-for line in lines:
-    count=perm(line["string"],0,line["blocks"],0)
-    print(count)
+def perm(string,strindex,blocks,blindex):
+    count=0
+    misses=set()
+    heap=deque()
+    heap.append((strindex,blindex))
+    while len(heap)>0:
+        s,b = heap.pop()
+        if (s,b) in misses:
+            continue
+        if b==0 and string[:s].count("#")>0:
+            print(f'shifted past first block')
+            continue
+        if sum(blocks[b:])+len(blocks)-b-1>len(string[s:]):
+            misses.add((s,b))
+            print(f'not enough string left:{s},{b} need {sum(blocks[b:])+len(blocks)-b-1}, got {len(string[s:])}')
+            continue
+        if b<len(blocks):
+            blocksize = blocks[b]
+            if b==len(blocks)-1:
+                matchstring="(?=(^[?#]{"+str(blocksize)+"})([.?]*$))"
+            else:
+                matchstring="(?=(^[?#]{"+str(blocksize)+"}[.?]))"
+            #print(s,b,matchstring,string[s:])
+            match=re.match(matchstring,string[s:])
+            if match:
+                #print(s,b,match.group(1),len(match.group(1)))
+                if b==len(blocks)-1:
+                    heap.append((s+blocksize+len(match.group(2)),b+1))
+                else:
+                    heap.append((s+blocksize+1,b+1))
+            if string[s]!="#":
+                heap.append((s+1,b))
+        elif string[s:].count("#")==0:
+            count+=1
+            #hits.add(st)
+        else:
+            #print(f'blocks left at end')
+            misses.add((s,b))
+            continue
+    return count
 
-li=["#","."]
 total=0
 for line in lines:
-    if line["string"][:line["blocks"][0]].count("#")==line["blocks"][0] or line["string"][-line["blocks"][-1]:].count("#")==line["blocks"][-1]:
-        print("blocks adjacent to beginning or end")
-        linea="."+line["string"]
-        lineb=line["string"]+"."
-    else:
-        linea="?"+line["string"]
-        lineb=line["string"]+"?"
-    lineablocks=line["blocks"].copy()
-    linebblocks=line["blocks"].copy()
-    # Test line a and b
-    candidatesab=[''.join(comb) for comb in product(li, repeat=line["blanks"]+1) if comb.count("#")==sum(lineablocks)-linea.count("#")]
-    candidates=[''.join(comb) for comb in product(li, repeat=(line["blanks"])) if comb.count("#")==sum(line["blocks"])-line["string"].count("#")]
-    print(line["string"],line["blocks"])
-    stringa="(^\.*"
-    for num in lineablocks:
-        stringa+='#{'+str(num)+'}'+'\.+'
-    stringa=stringa[:-3]+"\.*$)"
-    stringb="(^\.*"
-    for num in linebblocks:
-        stringb+='#{'+str(num)+'}'+'\.+'
-    stringb=stringb[:-3]+"\.*$)"
-    string="(^\.*"
-    for num in line["blocks"]:
-        string+='#{'+str(num)+'}'+'\.+'
-    string=string[:-3]+"\.*$)"
-    matchesa=0
-    for cand in candidatesab:
-        perm=""
-        i=0
-        for c in linea:
-            if c=="?":
-                perm+=cand[i]
-                i+=1
-            else:
-                perm+=c
-        match=re.search(stringa,perm)
-        if match:
-            matchesa+=1
-            print(perm,match.group(0))
-#        else:
-#            print(perm)
-    print(f'a {matchesa}')
-    matchesb=0
-    for cand in candidatesab:
-        perm=""
-        i=0
-        for c in lineb:
-            if c=="?":
-                perm+=cand[i]
-                i+=1
-            else:
-                perm+=c
-        match=re.search(stringb,perm)
-        if match:
-            matchesb+=1
-            print(perm,match.group(0))
-#        else:
-#            print(perm)
-    print(f'b {matchesb}')
-    matchesd=0
-    for cand in candidates:
-        perm=""
-        i=0
-        for c in line["string"]:
-            if c=="?":
-                perm+=cand[i]
-                i+=1
-            else:
-                perm+=c
-        match=re.search(string,perm)
-        if match:
-            matchesd+=1
-            print(perm,match.group(0))
-#        else:
-#            print(perm)
-    print(f'd {matchesd}')
-
-    if matchesa>matchesb:
-        matches=(matchesa**4)*matchesd
-    elif matchesb>matchesa:
-        matches=(matchesb**4)*matchesd
-    else:
-        matches=matchesd**5
-    print(matches)
-    total+=matches
+    misses=set()
+    cache={}
+    l=line["string"]+"?"+line["string"]+"?"+line["string"]+"?"+line["string"]+"?"+line["string"]
+    lb=[*line["blocks"],*line["blocks"],*line["blocks"],*line["blocks"],*line["blocks"]]
+    print(f'Trying {l} with blocks {lb}')
+    count=rec(l,0,lb,0)
+    total+=count
+    print(count)
 print(total)
+
